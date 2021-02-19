6b51cd6df17906269279ed8bef7219a11f1bf269
tanioka <tanioka.mail@gmail.com>
2021-02-19 10:57:09 +0900

    app完成形(vue.js)

---
+++ src/components/ShareMessage.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="share">
+    <p>シェア</p>
+    <textarea v-model="share"></textarea>
+    <div @click="send">
+      <button>シェアする</button>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  data() {
+    return {
+      share: "",
+    };
+  },
+  methods: {
+    send() {
+      if (this.share === "") {
+        alert("シェアする内容を入力してください");
+      } else {
+        axios
+          .post("herokuのURL/api/shares", {
+            user_id: this.$store.state.user.id,
+            share: this.share,
+          })
+          .then((response) => {
+            console.log(response);
+            alert("シェアしました");
+            this.share = "";
+            this.$router.go({
+              path: this.$router.currentRoute.path,
+              force: true,
+            });
+          });
+      }
+    },
+  },
+};
+</script>
+
+<style scoped>
+.share {
+  margin: 15px;
+}
+.share textarea {
+  width: 95%;
+  height: 120px;
+  margin-top: 15px;
+  margin-bottom: 15px;
+  border-radius: 10px;
+  border: 1px solid white;
+  background-color: #15202b;
+  color: white;
+  resize: none;
+}
+button {
+  width: 100px;
+  text-align: center;
+  padding: 8px 0 10px;
+  color: #fff;
+  background-color: #5419da;
+  border-radius: 25px;
+  display: block;
+  margin: 0 0 0 auto;
+}
+</style>
